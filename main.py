c41160210d65a22b68dd1eefc9e3faba3dbb9701
Stivodavco <david.stepitacademy@gmail.com>
2025-01-30 18:39:26 +0100

    I wrote all of the code, added a city - Petrzalka

---
+++ main.py
@@ -0,0 +1,14 @@
+class City:
+    def __init__(self,city_name,region_name,country_name,population,zip_code,area_code):
+        self.city_name = city_name
+        self.region_name = region_name
+        self.country_name = country_name
+        self.population = population
+        self.zip_code = zip_code
+        self.area_code = area_code
+
+    def get_full_adress(self):
+        return "{} {} ({}), {}.".format(self.city_name,self.zip_code,self.region_name,self.country_name)
+
+petrzalka = City("Petržalka","Bratislavský Kraj","Slovensko",114000,"851 01","+421 2")
+print(petrzalka.get_full_adress())
